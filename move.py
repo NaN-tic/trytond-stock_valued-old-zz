52eebc6ca5ed1452bfed4bf5454780ec13eb2342
Guillem Barba <guillem@nan-tic.com>
2014-06-04 13:08:51 +0200

    Remove __main__ entry point and PATH manipulation in test

--- move.py
+++ move.py
@@ -1,6 +1,5 @@
-# This file is part stock_valued module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 from decimal import Decimal
 
 from trytond.model import fields
--- tests/test_stock_valued.py
+++ tests/test_stock_valued.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python
-#This file is part stock_valued module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_depends, test_view
@@ -41,6 +32,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         StockValuedTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
